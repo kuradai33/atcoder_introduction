ff0db492c0bdde20b81be4066294dde15e5261bf
kuradai33 <106243129+kuradai33@users.noreply.github.com>
2025-04-21 00:33:06 +0900

    example1_ans.cppを追加

---
+++ example1_ans.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    // 変数を宣言する
+    int a, b, c;
+    std::string s;
+    // 入力する
+    std::cin >> a >> b >> c >> s;
+    // 処理を行う
+    int sum = a + b + c;
+    // 結果を出力する
+    std::cout << sum << " " << s << std::endl;
+}
