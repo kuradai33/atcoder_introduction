d37a4932ba129bd86993a2f6c3a643757e1d078a
kuradai33 <106243129+kuradai33@users.noreply.github.com>
2025-04-20 15:17:24 +0000

    sample.cppを追加

---
+++ sample.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    int number1, number2, sum;
+
+    std::cin >> number1 >> number2;
+
+    sum = number1 + number2;
+    std::cout << sum << std::endl;
+}
