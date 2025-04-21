87ca72eecbc3b88dc3c45e43815f4a400fd20b51
kuradai33 <106243129+kuradai33@users.noreply.github.com>
2025-04-21 15:26:13 +0900

    改行を追加

--- example1_ans.cpp
+++ example1_ans.cpp
@@ -5,10 +5,13 @@ int main(){
     // 変数を宣言する
     int a, b, c, sum;
     std::string s;
+    
     // 入力する
     std::cin >> a >> b >> c >> s;
+    
     // 処理を行う
     sum = a + b + c;
+    
     // 結果を出力する
     std::cout << sum << " " << s << std::endl;
 }
