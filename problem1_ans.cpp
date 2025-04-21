7399662a1c566193ac68357179c640bfcece853c
kuradai33 <106243129+kuradai33@users.noreply.github.com>
2025-04-21 20:03:39 +0900

    他の正しい条件式をコメントに追記

--- problem1_ans.cpp
+++ problem1_ans.cpp
@@ -9,18 +9,18 @@ int main(){
     std::cin >> X;
 
     // 処理を行う
-    if(X < 40){
+    if(X < 40){ // 0 <= X && X < 40 でも可
         rest = 40 - X;
     }
-    else if(X < 70){
+    else if(X < 70){ // 40 <= X && X < 70 でも可
         rest = 70 - X;
     }
-    else if(X < 90){
+    else if(X < 90){ // 70 <= X && X < 90 でも可
         rest = 90 - X;
     }
     
     // 結果を出力する
-    if(X < 90){
+    if(X < 90){ // 0 <= X && X < 90 でも可
         std::cout << rest << std::endl;
     }
     else{
