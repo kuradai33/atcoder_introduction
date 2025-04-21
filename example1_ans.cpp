c6675d56272ba6be19fb731fb9e2518f15ea533b
kuradai33 <106243129+kuradai33@users.noreply.github.com>
2025-04-21 10:05:46 +0900

    sumの宣言を移動

--- example1_ans.cpp
+++ example1_ans.cpp
@@ -3,12 +3,12 @@
 
 int main(){
     // 変数を宣言する
-    int a, b, c;
+    int a, b, c, sum;
     std::string s;
     // 入力する
     std::cin >> a >> b >> c >> s;
     // 処理を行う
-    int sum = a + b + c;
+    sum = a + b + c;
     // 結果を出力する
     std::cout << sum << " " << s << std::endl;
 }
