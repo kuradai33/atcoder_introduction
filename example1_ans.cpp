7970cadbc4c4f9fa0a866ebcfffa2a5563619801
kuradai33 <106243129+kuradai33@users.noreply.github.com>
2025-04-21 21:08:26 +0900

    sumの宣言場所を変更

--- example1_ans.cpp
+++ example1_ans.cpp
@@ -3,14 +3,14 @@
 
 int main(){
     // 変数を宣言する
-    int a, b, c, sum;
+    int a, b, c;
     std::string s;
     
     // 入力する
     std::cin >> a >> b >> c >> s;
     
     // 処理を行う
-    sum = a + b + c;
+    int sum = a + b + c;
     
     // 結果を出力する
     std::cout << sum << " " << s << std::endl;
