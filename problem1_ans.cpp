58eba98fd4f2a056fc39c3eac9f5a47ac6607a33
kuradai33 <106243129+kuradai33@users.noreply.github.com>
2025-04-21 15:29:58 +0900

    problem1_ans.cppを追加

---
+++ problem1_ans.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    // 変数を宣言する
+    int X, rest;
+
+    // 入力する
+    std::cin >> X;
+
+    // 処理を行う
+    if(X < 40){
+        rest = 40 - X;
+    }
+    else if(X < 70){
+        rest = 70 - X;
+    }
+    else if(X < 90){
+        rest = 90 - X;
+    }
+    
+    // 結果を出力する
+    if(X < 90){
+        std::cout << rest << std::endl;
+    }
+    else{
+        std::cout << "expert" << std::endl;
+    }
+}
