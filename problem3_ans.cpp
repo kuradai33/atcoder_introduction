bd0d9a88c19d7b7bc45c5d2b9684492a79d84c06
kuradai33 <106243129+kuradai33@users.noreply.github.com>
2025-04-22 15:59:37 +0900

    problem3_ans.cppを作成

---
+++ problem3_ans.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    // 変数を宣言する
+    long long int N, M, A[200000];
+
+    // 入力する
+    std::cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        std::cin >> A[i];
+    }
+
+    // 処理を行う
+    long long int sumA = 0;
+    for(int i = 0; i < N; i++){
+        sumA = sumA + A[i];
+    }
+    // 二分探索
+    long long int left = 0, right = 1000000001;
+    while(right - left > 1){
+        long long int mid = (left + right) / 2;
+
+        long long int sum = 0;
+        for(int i = 0; i < N; i++){
+            if(mid < A[i]){
+                sum = sum + mid;
+            }
+            else{
+                sum = sum + A[i];
+            }
+        }
+
+        if(sum <= M){
+            left = mid;
+        }
+        else{
+            right = mid;
+        }
+    }
+
+    // 結果を出力する
+    if(sumA > M){ // 答えが有限の場合
+        std::cout << left << std::endl;
+    }
+    else{ // 答えを無限に大きくできる場合
+        std::cout << "infinite" << std::endl;
+    }
+}
