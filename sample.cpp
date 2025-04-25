5fc3bb7a88e9d05679591ac34c6db5ed8a9bf49b
kuradai33 <106243129+kuradai33@users.noreply.github.com>
2025-04-25 17:56:35 +0900

    sumの定義場所を変更

--- sample.cpp
+++ sample.cpp
@@ -2,10 +2,10 @@
 #include <string>
 
 int main(){
-    int number1, number2, sum;
+    int number1, number2;
 
     std::cin >> number1 >> number2;
 
-    sum = number1 + number2;
+    int sum = number1 + number2;
     std::cout << sum << std::endl;
 }
